f086ddef694d4b64ea87e8c808f51403acb7e017
Ilya Andreev <ilyaa1982@gmail.com>
2018-05-26 16:27:17 -0400

    app.js updated

---
+++ app.js
@@ -0,0 +1,41 @@
+// Home
+$('#header').on('click', function (event) {
+    $('html, body').animate({
+        scrollTop: 0,
+    }, 'fast')
+})
+
+// About Me
+$('#nav-bio').on('click', function (event) {
+    $('html, body').animate({
+        scrollTop: $(".bio").offset().top
+    });
+})
+
+// Skills
+$('#nav-skills').on('click', function (event) {
+    $('html, body').animate({
+        scrollTop: $(".skills").offset().top
+    });
+})
+
+// Projects
+$('#nav-projects').on('click', function (event) {
+    $('html, body').animate({
+        scrollTop: $(".projects").offset().top
+    });
+})
+
+// Contact
+$('#nav-contact').on('click', function (event) {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top
+    });
+})
+
+// Back to top
+$('button').on('click', function (event) {
+    $('html, body').animate({
+        scrollTop: $(".pic").offset().top
+    });
+})
